8011d4ee2a1166715e64d49c05ff9a547efff988
Yusei <acmnhc@gmail.com>
2021-10-24 17:18:31 -0400

    Organizando el cors en el backend

---
+++ src/config/cors.config.js
@@ -0,0 +1,11 @@
+// Config cors and headers
+const cors = function (req, res, next)
+{
+    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+}
+
+module.exports = cors;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express 		= require('express');
 const morgan 		= require('morgan');
 const app 			= express();
-
+const cors            = require('./config/cors.config');
 const masterRouter 	= require('./apis/master.router');
 const server 		= require('./sockets/io.server')(app);
 const config 		= require('./config/config');
@@ -9,15 +9,7 @@ const config 		= require('./config/config');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
-
-// Config cors and headers
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
+app.use(cors);
 
 app.use('/api', masterRouter);
 
